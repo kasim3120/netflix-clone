a49b323f40363df92f5a3a6edd874942eaccd351
RainbowSwan <yam6931913@hotmail.com>
2022-08-08 15:46:23 +0800

    Updata page bug

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -46,7 +46,6 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
           // Logged in...
           setUser(user)
           setLoading(false)
-          router.push('/account')
         } else {
           // Not logged in...
           setUser(null)
